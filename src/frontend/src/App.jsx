e61e6d6e8a1dfbbbd1dba9979477e0e8bc7c84b8
Nathan Luong <luongn4@mcmaster.ca>
2024-10-29 03:44:15 -0400

    Login and Registration UI finished

---
+++ src/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import PredictionPage from "./pages/PredictionPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+function App() {
+  return (
+    <div className="bg-[#e1e1e1] w-screen h-screen overflow-hidden">
+      <RegisterPage />
+    </div>
+  );
+}
+
+export default App;
