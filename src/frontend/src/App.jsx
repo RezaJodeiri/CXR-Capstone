daefd0ac256ff469a040ea4c947d6365742d2bbd
Reza Jodeiri <Jodeirim@mcmaster.ca>
2025-01-03 20:46:35 -0500

    feat: Redesign UI

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import PredictionPage from "./pages/PredictionPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { AuthProvider } from "./context/Authentication";
 import { ProtectedRoute } from "./components/ProtectedRoute";
+import DashboardPage from "./pages/DashboardPage";
+import PatientsPage from "./pages/PatientsPage";
+import PatientDetailsPage from "./pages/PatientDetailsPage";
+import CreateMedicalRecord from "./components/Patient/CreateMedicalRecord";
 
 function App() {
+  const navigate = useNavigate();
+
   return (
     <AuthProvider>
       <div className="bg-[#e1e1e1] w-screen h-screen overflow-hidden">
@@ -18,8 +24,50 @@ function App() {
             path="dashboard" 
             element={
               <ProtectedRoute>
+                <DashboardPage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="prediction" 
+            element={
+              <ProtectedRoute>
                 <PredictionPage />
               </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="patients" 
+            element={
+              <ProtectedRoute>
+                <PatientsPage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="patients/:id" 
+            element={
+              <ProtectedRoute>
+                <PatientDetailsPage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="patients/:id/create-record" 
+            element={
+              <ProtectedRoute>
+                <PatientDetailsPage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/medical-records/:id" 
+            element={
+              <CreateMedicalRecord 
+                viewMode={true} 
+                onBack={() => navigate(-1)}
+                onRecordCreated={() => {}}
+              />
             } 
           />
         </Routes>
