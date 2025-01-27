60eb3053ef404a8df952f11e731ae6afe3def6a3
Kelly Deng <kd0819@Kellys-MacBook-Air.local>
2025-01-27 16:30:15 -0500

    Please enter the commit message for your changes. Lines starting

--- src/backend/runtime/data/aws_s3.py
+++ src/backend/runtime/data/aws_s3.py
@@ -7,7 +7,11 @@ s3_client = boto3.client('s3')
 # s3_client.create_bucket(Bucket='my-new-bucket')
 
 # Upload a file
-s3_client.upload_file('/Users/kd0819/Downloads/resume_kellydeng.pdf', 'plzplzplz', 'test.pdf')
+def get_file(local_file_path,filename):
+    #local_file_path = '/Users/kd0819/Downloads/test_local.pdf'
+    #filename - 'test.pdf'
+    bucket = 'plzplzplz'
+    s3_client.upload_file(local_file_path, bucket, filename)
 
 response = s3_client.list_buckets()
 print("Buckets:")
@@ -15,4 +19,9 @@ for bucket in response['Buckets']:
     print(bucket['Name'])
 
 # Download a file
-s3_client.download_file('plzplzplz', 'test.pdf', '/Users/kd0819/Downloads/test_local.pdf')
+def download_file(local_file_path,filename):
+    #local_file_path = '/Users/kd0819/Downloads/test_local.pdf'
+    #filename - 'test.pdf'
+    bucket = 'plzplzplz'
+    s3_client.download_file(bucket, filename, local_file_path)
+    print(f"File downloaded to {local_file_path}")
