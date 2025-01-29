14a5bab0c04e234a84408a98afac299c1b70250a
Kelly Deng <kd0819@Kellys-MacBook-Air.local>
2025-01-29 00:02:00 -0500

    using pre-assigned url to upload/download files to/from s3

--- src/backend/runtime/__init__.py
+++ src/backend/runtime/__init__.py
@@ -3,6 +3,7 @@ from .data.aws_cognito import CognitoIdentityProvider
 from .data.prediction_services import PredictionService as PS
 from .data.aws_dynamodb.medical_record_service import MedicalRecordService as MRS
 from .data.aws_dynamodb.medical_prescription_service import MedicalPrescriptionService as MPS
+from .data.aws_s3 import S3PresignedURLHandler as s3
 
 from .logger import Logger
 
@@ -17,5 +18,6 @@ IdentityProvider = CognitoIdentityProvider(
 PredictionService = PS()
 MedicalRecordService = MRS("capstone_medical_record", "us-west-2")
 MedicalPrescriptionService = MPS("capstone_medical_prescription", "us-west-2")
+S3Handler = s3('plzplzplz')
 
 RuntimeLogger = Logger().getLogger()
