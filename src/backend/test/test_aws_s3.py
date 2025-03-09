555732ff72cb94129974cf045ed0287fa1b266f2
Kelly Deng <kd0819@Kellys-MacBook-Air.local>
2025-03-09 00:03:38 -0500

    S3 unit test

--- src/backend/test/test_aws_s3.py
+++ src/backend/test/test_aws_s3.py
@@ -1,49 +1,26 @@
-import boto3
-import pytest
-import requests
-from moto import mock_s3
-from botocore.exceptions import NoCredentialsError
-from runtime import S3PresignedURLHandler
+from .mocks.mock_s3 import MockBoto3S3Client
 from unittest.mock import patch
 
-@pytest.fixture
-def mock_post(url,data,files):
-    response = {
-        "status_code": 204,
-        "headers": {
-            "x-amz-request-id": "example-request-id",
-            "x-amz-id-2": "example-amz-id",
-            "Content-Length": "0"
-        }
-        }
+class TestS3PresignedURLHandler():
 
-    return response 
+    @patch.object(MockBoto3S3Client, 'generate_presigned_url', return_value="https://mocks3.com")
+    def test_generate_upload_url(self,mock_generate_presigned_url):
+        """Test generating an S3 pre-signed upload URL."""
 
-
-
-@pytest.fixture
-def test_generate_upload_url():
-    """Test generating an S3 pre-signed upload URL."""
-
-    # Mock expected response
-    expected_response = {
-        "url": "https://mocked-s3-url.com",
-        "fields": {"key": "test_upload.txt"}
-    }
-
-    return expected_response
-
-@pytest.fixture
-def test_upload_file_success(self, file_path = "xxx/xxx/image.pdf"):
-    """Test file upload using mocked requests.post (for pre-signed URL upload)."""
-    presigned_data = self.test_generate_upload_url()
-
-    with open(file_path, 'rb') as file:
-        files = {'file': file}
-        response = mock_post(presigned_data['url'], data=presigned_data['fields'], files=files)
+        url = MockBoto3S3Client.generate_presigned_url()
+        assert isinstance(url, str)
     
-    assert response["status_code"] == 204
-    
+    @patch.object(MockBoto3S3Client, 'upload_file', return_value={"message": "File uploaded successfully", "status": 200})
+    def test_upload_url(self,mock_upload_file):
+        response = MockBoto3S3Client.upload_file()
+
+        assert(response["status"], 200)
+        
+
+        
+
+
+        
 
 
 
