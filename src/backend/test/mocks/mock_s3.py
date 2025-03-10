6ca849d4e113e95f5c98c5f73c138ba30d7a0507
Kelly Deng <kd0819@Kellys-MacBook-Air.local>
2025-03-09 21:04:30 -0400

    change based on comments

--- src/backend/test/mocks/mock_s3.py
+++ src/backend/test/mocks/mock_s3.py
@@ -4,7 +4,7 @@ from runtime.data.aws_s3 import S3PresignedURLHandler
 class MockS3PresignedURLHandler(S3PresignedURLHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.cognito_idp_client = MockBoto3S3Client()
+        self.s3client = MockBoto3S3Client()
 
 
 class MockBoto3S3Client(MagicMock):
@@ -12,7 +12,8 @@ class MockBoto3S3Client(MagicMock):
         super().__init__(*args, **kwargs)
 
     def generate_presigned_url(self):
-        return "https://mocks3.com"
+        return "https://mock-s3-bucket.s3.amazonaws.com/uploads/fake-file.txt?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=FAKECREDENTIAL%2F20250309%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250309T000000Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=FAKESIGNATURE1234567890"
+
     
     def upload_file(self):
         return {"message": "File uploaded successfully", "status": 200}
