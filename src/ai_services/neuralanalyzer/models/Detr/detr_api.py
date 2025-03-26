55795cb8c17a092ac398b37bf327913c017ea849
LongpanZhou <zhoul83@mcmaster.ca>
2025-03-26 03:48:27 -0400

    fix backend, rename folder

---
+++ src/ai_services/neuralanalyzer/models/Detr/detr_api.py
@@ -0,0 +1,81 @@
+import io
+import os
+import torch
+
+from PIL import Image, ImageDraw, ImageFont
+from src.ai_services.neuralanalyzer.models.Detr.detr import Detr
+
+# Constants
+MODEL_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "detr.ckpt")
+
+anatomical_regions = [
+    'right upper lung zone',
+    'right mid lung zone',
+    'right lower lung zone',
+    'right costophrenic angle',
+    'right hilar structures',
+    'right apical zone',
+    'left upper lung zone',
+    'left mid lung zone',
+    'left lower lung zone',
+    'left costophrenic angle',
+    'left hilar structures',
+    'cardiac silhouette',
+]
+
+color_dict = {
+    1: "red", 2: "green", 3: "purple", 4: "orange",
+    5: "brown", 6: "pink", 7: "blue", 8: "olive", 9: "cyan",
+    10: "magenta", 11: "yellow", 12: "black"
+}
+
+# Check Device
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available() and torch.backends.mps.is_built():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
+
+print(f"Using device: {device}")
+
+# Load model
+model = Detr.load_from_checkpoint(MODEL_PATH, map_location=device)
+model.to(device)
+model.eval()
+model = torch.compile(model)
+print("DETR Model loaded...")
+
+def image_segmentation(binary_data):
+    image = Image.open(io.BytesIO(binary_data))
+    image = image.convert('RGB')
+
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("arial.ttf", 40)
+
+    with torch.no_grad():
+        segmentations = model.get_boxes(image)
+
+    for segmentation in segmentations:
+        region, score, x1, y1, x2, y2 = segmentation.strip().split(" ")
+        region, score, x1, y1, x2, y2 = int(region), float(score), float(x1), float(y1), float(x2), float(y2)
+        draw.rectangle([x1, y1, x2, y2], outline=color_dict[region], width=5)
+        label = f" {anatomical_regions[region-1]}: {score:.2f}"
+        draw.text((x1, y1), text=label, fill=color_dict[region], font=font)
+
+    img_byte_arr = io.BytesIO()
+    image.save(img_byte_arr, format='JPEG')
+    img_byte_arr.seek(0)
+
+    image.save('test.jpg', "JPEG")
+    return img_byte_arr
+
+def get_features(binary_data):
+    image = Image.open(io.BytesIO(binary_data))
+    image = image.convert('RGB')
+
+    return model.get_features(image)
+
+tmp = "00005197-869d72f3-66210bf4-fa2c9d83-b613c4e7"
+img_binary = open(f"{tmp}.jpg", 'rb').read()
+image_segmentation(img_binary)
